9469e930c6db0e8391136d48a2c430b598edc0b0
Zaytsev Aleksandr <145544514+ZaytsevAlexandr@users.noreply.github.com>
2025-02-22 14:51:23 +0300

    Build library deleted

--- 2-CSR-Dense-Comparison/build/CMakeFiles/matrix_operations.dir/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/matrix_operations.dir/main.cpp.o: \
- /home/aobaks/SLAE/2-matrix-timecomparison/main.cpp \
- /usr/include/stdc-predef.h \
- /home/aobaks/SLAE/2-matrix-timecomparison/benchmark.h
